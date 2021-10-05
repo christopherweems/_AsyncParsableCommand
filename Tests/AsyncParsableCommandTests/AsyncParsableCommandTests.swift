dd9a413e180fc624d2336d52f60f00e021e3f2d3
Christopher Weems <hello@christopherweems.com>
2021-10-05 10:10:11 -0400

    Add `_AsyncParsableCommandTests.testExample()`

--- Tests/AsyncParsableCommandTests/AsyncParsableCommandTests.swift
+++ Tests/AsyncParsableCommandTests/AsyncParsableCommandTests.swift
@@ -1,6 +1,31 @@
 import XCTest
+import ArgumentParser
 @testable import AsyncParsableCommand
 
 final class _AsyncParsableCommandTests: XCTestCase {
+    func testExample() async throws {
+        struct Countdown: _AsyncParsableCommand {
+            @Argument var count: Int
+            
+            func run() async throws {
+                var remaining = count
+                
+                while 0 < remaining {
+                    print("\(remaining)!")
+                    await Task.sleep(1_000_000_000)
+                    remaining -= 1
+                }
+                
+                print("Countdown complete!")
+                
+            }
+            
+        }
+        
+        var countdown = Countdown()
+        countdown.count = 5
+        
+        try await countdown.run()
+    }
     
 }
